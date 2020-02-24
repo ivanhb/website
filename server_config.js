981ddd3c2e452a9e500903b89bfb252c5170c2a0
ivan.heibi <ivanhb.ita@gmail.com>
2020-02-24 17:37:56 +0100

    server config

--- server_config.js
+++ server_config.js
@@ -1,5 +1,4 @@
 /*--- IN CASE IS ON SERVER
-layout_config["homelogo"]["link"] = "data/img/full_logo_vanilla.svg";
 layout_config["homepage"] = "website/";
 layout_config["pages_dir"] = "website/page/";
 */
